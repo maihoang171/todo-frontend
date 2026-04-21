126235eec6ee5904b13aafbe77cc290ee216a5ff
Jayce <maihoang171@gmail.com>
2026-04-21 21:16:08 +0700

    update axios url

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const axiosClient = axios.create({
   timeout: 60000,
   withCredentials: true,
-  baseURL: import.meta.env.VITE_BE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1'
 });
